2b25966cc3ca6705b3aa987116c4ae4fe5bd4a28
Jordan Marsh <jordanmarshdesign@gmail.com>
2018-10-30 18:01:33 -0500

    Updates

---
+++ precache-manifest.f42f67761bae5fb69bb9067faa061842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "e758863633eba7876ad3",
+    "url": "/memory-game/static/js/main.e7588636.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/memory-game/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "e758863633eba7876ad3",
+    "url": "/memory-game/static/css/main.372b9020.chunk.css"
+  },
+  {
+    "revision": "5ac032b8ce89a32d01ddfe9180474804",
+    "url": "/memory-game/index.html"
+  }
+];
